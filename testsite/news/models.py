218e57de1b386b72b2c0b3e8f4c38e209e50648a
Alina Shcherbinina <alinkash@gmail.com>
2022-10-25 15:57:07 +0800

    work with categories

--- testsite/news/models.py
+++ testsite/news/models.py
@@ -2,13 +2,30 @@ from django.db import models
 
 # Create your models here.
 class News(models.Model):
-    title = models.CharField(max_length=150)
+    title = models.CharField(max_length=150, verbose_name='article title')
     content = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
-    is_published = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='publised at')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='updated at')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    is_published = models.BooleanField(default=True, verbose_name='published?')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null = True)
 
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name = 'article'
+        # verbose_name_plural = 'articles'
+        ordering =['-created_at']
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, db_index=True, verbose_name='category name')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+        ordering =['title']
--- testsite/news/views.py
+++ testsite/news/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import News
+from .models import News, Category
 
 # Create your views here.
 def index(request):
     # print(dir(request))
-    news = News.objects.order_by('-created_at')
+    news = News.objects.all()
+    categories = Category.objects.all()
     context = {
         'news': news,
-        'title': 'News selection'
+        'title': 'News selection',
+        'categories': categories
     }
     return render(request, 'news/index.html', context)
+
+def get_category(request, category_id):
+    news = News.objects.filter(category_id=category_id)
+    categories = Category.objects.all()
+    category = Category.objects.get(pk=category_id)
+    return render(request, template_name='news/category.html', context={'news': news, 'categories': categories, 'category': category} )
+
 
